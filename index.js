c77dd521b5a6a0ea3a601c7a82068906dc5e0e7c
RoxanneGutierrez <roxannenguyenn@gmail.com>
2022-10-15 12:30:25 +0200

    commit tree d802fcea05e74d93b717a3476687cf049411acbd

---
+++ index.js
@@ -0,0 +1,37 @@
+function add(a, b) {
+    const result = a + b
+    return result
+}
+
+function subtract(a, b) {
+    const result = a - b
+    return result
+}
+
+function multiply(a, b) {
+    const result = a * b
+    return result
+}
+
+function divide(a, b) {
+    const result = a / b
+    return result
+}
+
+function increment(n) {
+    n++
+    return n
+}
+
+function decrement(n) {
+    n--
+    return n
+}
+
+function makeInt(n) {
+    return parseInt(n, 10)
+} 
+
+function preserveDecimal(n) {
+    return parseFloat(n)
+}
